421168ffe20179a0326c41a3087ef6c2e0cf2133
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-19 22:03:30 +0500

    Initial Commit

---
+++ MiliushaHwOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiliushaHwOne
+//
+//  Created by Михаил Милюша on 19.08.2020.
+//  Copyright © 2020 Mikhail Miliusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
