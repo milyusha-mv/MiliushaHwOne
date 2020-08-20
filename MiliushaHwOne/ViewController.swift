82fa7f8a48a3062a38cb8a7b3907b12ef3d06a57
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-20 20:50:01 +0500

    make some changes

--- MiliushaHwOne/ViewController.swift
+++ MiliushaHwOne/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var lightButton: UIButton!
+    var nextColor = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 80
+        yellowView.layer.cornerRadius = 80
+        greenView.layer.cornerRadius = 80
+        lightButton.layer.cornerRadius = 10
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
     }
 
-
+    @IBAction func turnOnLoghtButtonPressed() {
+        
+        switch nextColor {
+        case "Red":
+            nextColor = "Yellow"
+            redView.alpha = 1
+            greenView.alpha = 0.3
+            lightButton.setTitle("Включить желтый", for: .normal)
+        case "Yellow":
+            nextColor = "Green"
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            lightButton.setTitle("Включить зелёный", for: .normal)
+        case "Green":
+            nextColor = "Red"
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            lightButton.setTitle("Включить красный", for: .normal)
+        default:
+            nextColor = "Yellow"
+            redView.alpha = 1
+            lightButton.setTitle("Включить жёлтый", for: .normal)
+        }
+    }
 }
-
